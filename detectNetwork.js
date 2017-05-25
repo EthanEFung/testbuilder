c47d1cbbab6066ff59d85fd2a011d8ffd92eb4d1
Ethan Fung <EthanEFung@gmail.com>
2017-05-25 14:59:45 -0700

    Define cardTypes list, and functional detectNetwork
    Attempting for a second time to implement a working solution
    given that the first solution did not pass.  Hoping to catch
    minor details that render test-builder broken. First
    instantiated cardTypes array, which the detectNetwork
    function will iterate through.  Wrote the same functional
    algorithm that remains flexible for the remaining credit
    cards.

--- detectNetwork.js
+++ detectNetwork.js
@@ -6,8 +6,33 @@
 // There are two indicators:
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
+var cardTypes = [
+
+{
+  network: "Diner's Club",
+  prefixes: [38, 39],
+  numOfDigits: [14]
+},
+
+{
+  network: "American Express",
+  prefixes: [34, 37],
+  numOfDigits: [15]
+}
+
+];
 
 var detectNetwork = function(cardNumber) {
+  return cardTypes.reduce((validCard, card) => {
+    var prefixMatches = card.prefixes.some((prefix) => {
+      prefix = prefix.toString();
+      return prefix === cardNumber.slice(0, prefix.length);
+    });
+
+    var lengthMatches = card.numOfDigits.some((numOfDigits) => numOfDigits === cardNumber.length);
+
+    return prefixMatches && lengthMatches ? card.network : validCard;
+  }, "Invalid Card");
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
